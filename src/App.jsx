13d0c09510d43ae4434ca5f56958420ab7bfd1f5
germ <29132034+lowell1@users.noreply.github.com>
2022-09-09 14:32:34 -0700

    style update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+
+export default function App() {
+  return (
+    // <div>
+    <div id="app">
+      <textarea placeholder="write code here" />
+      <textarea placeholder="output" />
+      <button>execute</button>
+    </div>
+    // </div>
+  );
+}
