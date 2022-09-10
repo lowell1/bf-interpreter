5ebc214a80f8fd5b91f3dfd247086b9600d7ba15
germ <29132034+lowell1@users.noreply.github.com>
2022-09-10 16:27:58 -0700

    update body styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import "./app.css";
 
 export default function App() {
   return (
-    // <div>
     <div id="app">
       <textarea placeholder="write code here" />
       <textarea placeholder="output" />
       <button>execute</button>
     </div>
-    // </div>
   );
 }
